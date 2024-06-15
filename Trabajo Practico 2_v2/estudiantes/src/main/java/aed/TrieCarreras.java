e65435366c9b0471a18638a548401ef1793c44ff
frannnnuba <plottier2002@gmail.com>
2024-06-15 19:22:14 -0300

    actualizacion

--- Trabajo Practico 2_v2/estudiantes/src/main/java/aed/TrieCarreras.java
+++ Trabajo Practico 2_v2/estudiantes/src/main/java/aed/TrieCarreras.java
@@ -1,6 +1,6 @@
 package aed;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class TrieCarreras<T> {
 
@@ -9,7 +9,7 @@ public class TrieCarreras<T> {
 
     private class nodoCarreras{
         ArrayList<nodoCarreras> hijos;
-        Materia                 materiaDeCarrera;
+        Materias                materiasDeCarrera;
     }
 
     public ArrayList<nodoCarreras> nodoCarreras(int espacio){
@@ -20,4 +20,38 @@ public class TrieCarreras<T> {
         this.raiz           = new nodoCarreras();
         this.cantidadNodos  = 0;
     }
+
+    public boolean buscar (String palabra) {
+        nodoCarreras nodoActual = raiz;
+
+        for (int i = 0; i < palabra.length(); i++) {
+            char caracter   = palabra.charAt(i);            //charAT(i) devuelve el char en la posición i
+            int indice      = (int) caracter;
+            
+            if(nodoActual.hijos.get(indice) == null){
+                return false;
+            }
+            nodoActual = nodoActual.hijos.get(indice);      //get(indice) devuelve el hijo en la posición indice
+        }                                                   //en este caso, es el nodo en la posición indice del array
+        return nodoActual != null && (nodoActual.materiasDeCarrera != null);
+    }
+
+    public void agregar (String palabra) {
+        nodoCarreras nodoActual = raiz;
+
+        for (int i = 0; i < palabra.length(); i++) {
+            char caracter   = palabra.charAt(i);
+            int indice      = (int) caracter;
+            
+            if (nodoActual.hijos.get(indice) != null){
+                nodoActual = nodoActual.hijos.get(indice);
+            }else{
+                nodoCarreras nuevoNodo  = new nodoCarreras();
+                nodoActual.hijos.set(indice, nuevoNodo);
+                nodoActual              = nuevoNodo;
+            }
+        
+        }
+    }
 }
+
--- Trabajo Practico 2_v2/estudiantes/src/main/java/aed/TrieMaterias.java
+++ Trabajo Practico 2_v2/estudiantes/src/main/java/aed/TrieMaterias.java
@@ -1,11 +1,69 @@
 package aed;
 
-import java.util.*;
+import java.util.ArrayList;
+
+import sun.net.www.protocol.http.spnego.NegotiatorImpl;
 
 public class TrieMaterias{
+    private NodoMaterias raiz;
+    private NodoMaterias siguiente;
+    private Materias materiasDeCarrera;
+
     private class NodoMaterias{
+        ArrayList<NodoMaterias> hijosmaterias; // Array que va a tener los hijos que diferencian materias
+        ArrayList<NodoMaterias> valorActual;
+        public NodoMaterias(){ // constructor del nodo 
+            this.valorActual = new ArrayList<NodoMaterias>(256);
+            this.hijosmaterias = new ArrayList<NodoMaterias>(256); //le declaramos el array con hijos
+        }
 
     }
-
+    // Constructor del trie
+    public TrieMaterias(){
+        this.raiz = new NodoMaterias();
+        this.siguiente = new NodoMaterias();
+        this.materiasDeCarrera = new Materias();
+    }
     
-} 
+    //nos fijamos si existe la palabra 
+    public boolean pertenece (String palabra) {
+        NodoMaterias actual = raiz; //accedemos a la raiz del trie
+        for (int i = 0; i < palabra.length(); i++) { // 
+            char caracter   = palabra.charAt(i);
+            int indice = (int) caracter;
+            
+            if(actual.hijosmaterias.get(indice) == null){
+                return false;
+            }
+            actual = actual.hijosmaterias.get(indice);
+        }
+        return actual != null && (materiasDeCarrera != null);
+    }
+
+
+    public void agregar(String palabra){
+        NodoMaterias actual = raiz;
+        for (int i = 0; i < palabra.length(); i++) {
+            char caracter   = palabra.charAt(i);
+            int indice      = (int) caracter;
+            
+            if (actual.hijosmaterias.get(indice) != null){
+                actual = actual.hijosmaterias.get(indice);
+            }else{
+                NodoMaterias nuevoNodo  = new NodoMaterias();
+                actual.hijosmaterias.set(indice, nuevoNodo);
+                actual = nuevoNodo;
+            }
+        }
+    }
+
+    public void eliminar(String palabra){
+        NodoMaterias actual = raiz;
+        if (pertenece(palabra)){
+            for (int i = 0; i < palabra.length(); i++){
+                
+            }
+        }
+    }
+
+}
