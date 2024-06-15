dc47a69088d8fa405c1cf81039f787d70b6b3a04
Gabriel Lopez <gabriellopezdu@gmail.com>
2024-06-15 14:05:08 -0300

    template TrieCarreras

---
+++ Trabajo Practico 2_v2/estudiantes/src/main/java/aed/TrieCarreras.java
@@ -0,0 +1,15 @@
+package aed;
+
+import java.util.*;
+
+public class TrieCarreras<T> {
+
+    private nodoTrie raiz;
+    private int nodos;
+
+    private class nodoTrie{
+        ArrayList<nodoTrie> hijos;
+
+    }
+
+}
